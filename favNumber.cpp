253540c7b8cd730b22232e32abef63b2e41cfba5
TravisAshcraft <58603687+TravisAshcraft@users.noreply.github.com>
2020-10-03 10:21:58 -0500

    removed some whitespace

--- favNumber.cpp
+++ favNumber.cpp
@@ -2,14 +2,13 @@
 
 
 int main() {
+
 	int favoriteNumber;
-	
 
 	std::cout << "What is your favorite number?" << std::endl;
 	std::cin >> favoriteNumber;
 
 	std::cout << "Wow " << favoriteNumber << " is not my favorite number,"
 	" my favorite number is " << favoriteNumber + 3 << std::endl;
-	
 
 }
